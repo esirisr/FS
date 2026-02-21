6265518e39915bfbbbe0990975bbf7f59a63bb3d
yyyyyy <esirsir123@gmail.com>
2026-02-21 19:40:55 +0300

    Update API base URL for production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
-const API = axios.create({ baseURL: 'https://hbe-production.up.railway.app/api' });
+const API = axios.create({ baseURL: 'tsbe1-production.up.railway.app/api' });
 
 
 // ---- Attach JWT automatically ----
